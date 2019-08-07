ef126b35e1641cd2ae77c73559acd2b20f214cc3
Nisal Fernando <nisal.shiroma86@gmail.com>
2019-08-07 12:01:21 +0200

    Added the ball

--- pongGame.js
+++ pongGame.js
@@ -1,5 +1,10 @@
 const canvas = document.querySelector("#pong");
 const context = canvas.getContext("2d");
 
+// Background color
 context.fillStyle = "#000";
 context.fillRect(0, 0, canvas.clientWidth, canvas.height);
+
+// Ball color
+context.fillStyle = "#fff";
+context.fillRect(0, 0, 10, 10);
