2c23aa3af3ae0e039029060a8dae396d1f5ff140
Nisal Fernando <nisal.shiroma86@gmail.com>
2019-08-09 13:32:41 +0200

    Ball is bouncing now

--- pongGame.js
+++ pongGame.js
@@ -45,8 +45,13 @@ function update(dt) {
     ball.pos.y += ball.vel.y * dt;
 
     // Bouncing the ball(x position)
-    if (ball.x < 0 || ball.x > canvas.width) {
+    if (ball.pos.x < 0 || ball.pos.x > canvas.width) {
         ball.vel.x = -ball.vel.x;
+    }
+
+    // Bouncing the ball(y position)
+    if (ball.pos.y < 0 || ball.pos.y > canvas.height) {
+        ball.vel.y = -ball.vel.y;
     }
 
     // Background color
