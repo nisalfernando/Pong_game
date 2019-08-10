2aee149777fe234f6420f40d4ae6799acee7b883
Nisal Fernando <nisal.shiroma86@gmail.com>
2019-08-10 12:39:31 +0200

    Added getters to the top and bottom

--- pongGame.js
+++ pongGame.js
@@ -19,6 +19,14 @@ class Rect {
     get right() {
         return this.pos.x + this.size.x / 2;
     }
+
+    get top() {
+        return this.pos.y - this.size.y / 2;
+    }
+
+    get bottom() {
+        return this.pos.y + this.size.y / 2;
+    }
 }
 
 class Ball extends Rect {
