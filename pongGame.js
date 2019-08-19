d2f635dd8e924772833078ac0316173d968b2239
Nisal Fernando <nisal.shiroma86@gmail.com>
2019-08-19 12:55:42 +0200

    Gave random starting direction to the ball

--- pongGame.js
+++ pongGame.js
@@ -109,8 +109,8 @@ class Pong {
 
     start() {
         if (this.ball.vel.x === 0 && this.ball.vel.y === 0) {
-            this.ball.vel.x = 300;
-            this.ball.vel.y = 300;
+            this.ball.vel.x = 300 * (Math.random() > 0.5 ? 1 : -1);
+            this.ball.vel.y = 300 * (Math.random() * 2 - 1);
         }
     }
 
