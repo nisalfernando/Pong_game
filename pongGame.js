1eb8ce491e24ecb9c9ae955b58abe1a8413cf0c4
Nisal Fernando <nisal.shiroma86@gmail.com>
2019-08-19 12:47:44 +0200

    Created an event listener to the start method

--- pongGame.js
+++ pongGame.js
@@ -145,3 +145,7 @@ const pong = new Pong(canvas);
 canvas.addEventListener("mousemove", event => {
     pong.players[0].pos.y = event.offsetY;
 });
+
+canvas.addEventListener("click", event => {
+    pong.start();
+});
